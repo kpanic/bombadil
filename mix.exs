a9eea4e99aff8c9a7b8aeaee463266b1af76b3ae
Marco Milanesi <kpanic@gmail.com>
2019-09-21 20:05:41 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bombadil.MixProject do
   def project do
     [
       app: :bombadil,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8",
       description: description(),
       package: package(),
